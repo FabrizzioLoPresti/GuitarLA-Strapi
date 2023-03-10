22e8882066525d819465a6625d259e25e64f21f0
Fabrizzio Lo Presti <89172392+FabrizzioLoPresti@users.noreply.github.com>
2023-03-10 16:58:34 -0300

    Add files via upload

---
+++ BDSTRAPI/cursos_202303101657.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.cursos (titulo,contenido,created_at,updated_at,published_at,created_by_id,updated_by_id) VALUES
+	 ('Cursos con hasta 30% de descuento','Donec ac venenatis elit, tincidunt mattis est. Suspendisse potenti. Praesent sed blandit lectus, at laoreet ligula. Morbi interdum et nibh ut efficitur.','2023-03-02 12:14:34.225','2023-03-02 12:14:42.505','2023-03-02 12:14:42.132',1,1);
